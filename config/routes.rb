aea10b1b1ace523b799557344482db300d9b49cb
alison frederic <alisonfrederic2@gmail.com>
2023-05-26 16:06:24 +0400

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists, only: %i[new create show index] do
+    resources :bookmarks, only: %i[new create]
+    # movies do
+    # get :title
+  end
 end
